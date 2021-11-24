11f112dab3d8901030fdadbafc399b95aa85e249
Samuel Gibson <sgibber2018@gmail.com>
2021-11-23 22:57:35 -0800

    PrisonersDilemmaGame.kt
    Upgraded PrisonersDilemmaGame to handle the AI players.

--- src/main/kotlin/PrisonersDilemmaGame.kt
+++ src/main/kotlin/PrisonersDilemmaGame.kt
@@ -7,41 +7,59 @@ const val punishmentPayoff = 2
 const val temptationPayoff = 0
 const val suckersPayoff = 3
 
+enum class PrisonersDilemmaPlayerLabel {
+    PLAYER_A,
+    PLAYER_B
+}
+
 enum class DilemmaChoice {
     COOPERATE,
     DEFECT
 }
 
-data class PrisonersDilemmaGameResult(
+data class PrisonersDilemmaRoundResult(
     val playerAScore: Int,
+    val playerAChoice: DilemmaChoice,
     val playerBScore: Int,
+    val playerBChoice: DilemmaChoice,
     val roundsPassed: Int,
-    val final: Boolean = false
+)
+
+data class PrisonersDilemmaGameResult(
+    val roundResults: List<PrisonersDilemmaRoundResult>,
+    val playerATotalScore: Int,
+    val playerBTotalScore: Int,
 )
 
 /**
  * A rendition of the Prisoner's Dilemma game.
  *
  * TODO: Some unit tests.
- * TODO: A means of inputting AI players with custom rule sets.
  * TODO: Try to reproduce the results of Holland's paper, where the AIs rediscover the tit-for-tat strategy and beyond.
  * TODO: Migrate to its own package, as the Genetic Playground should be able to handle any number of games designed
  *  to use it.
  * TODO: Extended rule sets involving more players. I'm sure Wikipedia can offer a plethora of such things.
  */
-class PrisonersDilemma(
-    private val roundsToPlay: Int,
-    private val interactiveMode: Boolean = false
-    // TODO: Behavior rule sets will go here, in this game and others.
+class PrisonersDilemmaGame(
+    val roundsToPlay: Int,
+    private val interactiveMode: Boolean = false,
+    private val playerA: PrisonersDilemmaPlayer,
+    private val playerB: PrisonersDilemmaPlayer,
 ) {
-    private var roundsPassed = 0
-    private var playerAScoreTotal = 0
-    private var playerBScoreTotal = 0
+    var roundsPassed = 0
+    var previousRounds = listOf<PrisonersDilemmaRoundResult>()
+    var playerAScoreTotal = 0
+    var playerBScoreTotal = 0
+
+    init {
+        playerA.playerLabel = PrisonersDilemmaPlayerLabel.PLAYER_A
+        playerB.playerLabel = PrisonersDilemmaPlayerLabel.PLAYER_B
+    }
 
     /**
      * Interactive mode exists for manual testing.
      */
-    private fun makeChoice(): DilemmaChoice {
+    private fun makeChoice(playerLabel: PrisonersDilemmaPlayerLabel): DilemmaChoice {
         return when (interactiveMode) {
             true -> {
                 println("(c)ooperate or (d)efect?")
@@ -52,45 +70,57 @@ class PrisonersDilemma(
                     else -> DilemmaChoice.values().random()
                 }
             }
-            else -> DilemmaChoice.values().random() // placeholder for rule sets describing behavior
+            else -> when (playerLabel) {
+                PrisonersDilemmaPlayerLabel.PLAYER_A -> playerA.chooseMove(this)
+                PrisonersDilemmaPlayerLabel.PLAYER_B -> playerB.chooseMove(this)
+            }
         }
     }
 
     /**
      * Plays a single round and returns the result.
      */
-    private fun playRound(): PrisonersDilemmaGameResult {
+    private fun playRound(): PrisonersDilemmaRoundResult {
         if (interactiveMode)
             println("Player A make your choice:")
-        val playerAChoice = makeChoice()
+        val playerAChoice = makeChoice(PrisonersDilemmaPlayerLabel.PLAYER_A)
         if (interactiveMode)
             println("Player B make your choice:")
-        val playerBChoice = makeChoice()
-        return when (Pair(playerAChoice, playerBChoice)) {
-            Pair(DilemmaChoice.COOPERATE, DilemmaChoice.COOPERATE) -> PrisonersDilemmaGameResult(
+        val playerBChoice = makeChoice(PrisonersDilemmaPlayerLabel.PLAYER_B)
+        val roundResult = when (Pair(playerAChoice, playerBChoice)) {
+            Pair(DilemmaChoice.COOPERATE, DilemmaChoice.COOPERATE) -> PrisonersDilemmaRoundResult(
                 playerAScore = rewardPayoff,
+                playerAChoice = DilemmaChoice.COOPERATE,
                 playerBScore = rewardPayoff,
+                playerBChoice = DilemmaChoice.COOPERATE,
                 roundsPassed = roundsPassed
             )
-            Pair(DilemmaChoice.DEFECT, DilemmaChoice.DEFECT) -> PrisonersDilemmaGameResult(
+            Pair(DilemmaChoice.DEFECT, DilemmaChoice.DEFECT) -> PrisonersDilemmaRoundResult(
                 playerAScore = punishmentPayoff,
+                playerAChoice = DilemmaChoice.DEFECT,
                 playerBScore = punishmentPayoff,
+                playerBChoice = DilemmaChoice.DEFECT,
                 roundsPassed = roundsPassed
             )
-            Pair(DilemmaChoice.COOPERATE, DilemmaChoice.DEFECT) -> PrisonersDilemmaGameResult(
+            Pair(DilemmaChoice.COOPERATE, DilemmaChoice.DEFECT) -> PrisonersDilemmaRoundResult(
                 playerAScore = suckersPayoff,
+                playerAChoice = DilemmaChoice.COOPERATE,
                 playerBScore = temptationPayoff,
+                playerBChoice = DilemmaChoice.DEFECT,
                 roundsPassed = roundsPassed
             )
-            Pair(DilemmaChoice.DEFECT, DilemmaChoice.COOPERATE) -> PrisonersDilemmaGameResult(
+            Pair(DilemmaChoice.DEFECT, DilemmaChoice.COOPERATE) -> PrisonersDilemmaRoundResult(
                 playerAScore = temptationPayoff,
+                playerAChoice = DilemmaChoice.DEFECT,
                 playerBScore = suckersPayoff,
+                playerBChoice = DilemmaChoice.COOPERATE,
                 roundsPassed = roundsPassed
             )
             else -> error("This should never happen.")
-            // ^ seems to be a bug. It complained that my conditions weren't exhaustive when they were. Perhaps
-            //  this is due to it not taking the nature of the Pair() in to account.
         }
+        playerA.averageScore += roundResult.playerAScore
+        playerB.averageScore += roundResult.playerBScore
+        return roundResult
     }
 
     /**
@@ -104,11 +134,14 @@ class PrisonersDilemma(
                 roundsPassed++
             }
         }
+
+        playerA.averageScore /= roundsPassed
+        playerB.averageScore /= roundsPassed
+
         return PrisonersDilemmaGameResult(
-            playerAScore = playerAScoreTotal,
-            playerBScore = playerBScoreTotal,
-            roundsPassed = roundsPassed,
-            final = true
+            roundResults = previousRounds,
+            playerATotalScore = playerAScoreTotal,
+            playerBTotalScore = playerBScoreTotal
         )
     }
 }
