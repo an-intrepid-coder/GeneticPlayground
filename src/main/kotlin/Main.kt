5117142ef9fc2ea0757df2427b3a04cbb6cfbcc0
Samuel Gibson <sgibber2018@gmail.com>
2021-11-26 14:04:34 -0800

    Main.kt
    Added some helpful commentary and set it to the default I used for the last time I ran it.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,8 +5,25 @@ import kotlinx.coroutines.coroutineScope
  * Currently, only runs Prisoner's Dilemma.
  * Will do more eventually.
  *
- * todo: args & a front-end
+ * todo: args & a front-end w/ a nice progress screen that shows the evolution of data over time. I'll use
+ *  Compose Multiplatform for that eventually, but in the interim (for at least the next few weeks), I will mostly
+ *  be printing either to screen (as of this writing) or to files which can be fed into graph viz software (next up).
  */
 suspend fun main(args: Array<String>) = coroutineScope {
-    PrisonersDilemmaPlayground().runExperiment(this)
+    /*
+        Note that the number of generations can have a very large effect. Initially, the gene pool will be
+        very random and so obviously good strategies will prevail. However, as it gets in to several hundred
+        generations and more, surprising and unexpected things begin to happen. Because they are competing
+        against each other (and lack a real control group), strategies which become favorable can depend heavily
+        on the quality (and nature) of the competition even more than on the rules of the game. This is especially
+        interesting when the number of rounds is high (100+, so that two players can really get the most out of each
+        other) and the gene pool size very large (10k+).
+     */
+    PrisonersDilemmaPlayground().runExperiment(
+        coroutineScope = this,
+        numGenerations = 1000,
+        numRounds = 300,
+        genePoolSize = 32000,
+        progressAlerts = PrisonersDilemmaPlayground.ProgressAlertType.LIGHT,
+    )
 }
