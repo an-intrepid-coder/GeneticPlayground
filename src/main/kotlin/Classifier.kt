c407a293e7e1ef16578553cc16f7a20b09154b1c
Samuel Gibson <sgibber2018@gmail.com>
2021-11-26 14:03:29 -0800

    Classifier.kt
    The Classifier now keeps track of its average score. This is assuming that all future instances of GeneticPlayground will use the same kind of fitness test -- which I'm not sure about, so this may change again in the future. Also added some helper functions and did a little refactoring.

--- src/main/kotlin/Classifier.kt
+++ src/main/kotlin/Classifier.kt
@@ -2,26 +2,55 @@
  * The "chromosome" of the Classifier system. It consists of a list of Characteristics and the means to "reproduce".
  * It can be sub-classed for more specific purposes.
  */
-abstract class Classifier( // todo: test
+abstract class Classifier(
     val characteristics: List<Characteristic>,
+    var averageScore: Double = 0.0,
 ) {
-    // todo: test
     abstract fun combine(other: Classifier): List<Classifier>
+    // TODO: ^ This is better off as an open fun if possible. There is definitely a default way this should work.
 
-    // todo: test
     fun asBinaryString(): String {
         var bitString = ""
-        characteristics.forEach { characteristic ->
-            bitString += when (characteristic.active) {
-                true -> "1"
-                else -> "0"
-            }
-        }
+        characteristics.forEach { bitString += it.asBitString() }
         return bitString
+    }
+
+    /**
+     * Returns how many instances of a given gene are active in a Classifier:
+     */
+    fun hasActiveGene(geneName: String): Boolean {
+        return characteristics.any { it.name == geneName && it.active }
     }
 }
 
-// todo: test
+/**
+ * Returns the number of Classifiers within a generation which are above the average score.
+ */
+fun numAboveAverage(
+    generation: List<Classifier>,
+    averageScore: Double,
+): Int {
+    return generation
+        .filter { it.averageScore > averageScore }
+        .size
+}
+
+/**
+ * Returns a string describing the most effective Classifier of the generation.
+ */
+fun championString(generation: List<Classifier>): String {
+    val champion = generation.maxByOrNull { it.averageScore }!!
+    val numOfChampion = generation.filter { it.asBinaryString() == champion.asBinaryString() }.size
+    val percentGenePool = numOfChampion.toDouble() / generation.size * 100
+    return "Champion: ${champion.asBinaryString()}" +
+            "\n${champion.characteristics.map { it.prettyPrint() }}" +
+            "\n\tScore: ${champion.averageScore}" +
+            "\n\t% of Gene Pool: $percentGenePool"
+}
+
+/**
+ * Takes a List<Classifier> and returns how many of them contain the active gene.
+ */
 fun numWithActiveGene(generation: List<Classifier>, geneName: String): Int {
     return generation.asSequence()
         .map { it.characteristics }
@@ -30,7 +59,8 @@ fun numWithActiveGene(generation: List<Classifier>, geneName: String): Int {
         .size
 }
 
-// todo: test
+// TODO: I can probably combine the following two into something cleaner:
+
 fun activeGenePercentages(generation: List<Classifier>): Map<String, Double> {
     val percentages = mutableMapOf<String, Double>()
     generation.forEach { classifier ->
