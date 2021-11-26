ce9fe84fa78553165e7b036b0160fd5c07d42521
Samuel Gibson <sgibber2018@gmail.com>
2021-11-26 14:13:19 -0800

    Random.kt
    Implemented a coinFlip function.

--- src/main/kotlin/Random.kt
+++ src/main/kotlin/Random.kt
@@ -1,10 +1,11 @@
-// Holland's paper suggests a mutation frequency of 1/10,000, which will be the default.
-const val defaultMutationFrequency = 10000
-
 /**
  * Returns true with a frequency of chanceOf/outOf.
  */
 fun withChance(outOf: Int, chanceOf: Int): Boolean {
     return (0..outOf).random() < chanceOf
+}
+
+fun coinFlip(): Boolean {
+    return withChance(1, 1)
 }
 
