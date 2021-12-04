cc77d0c29771e3900425e7de94eac7fec0020089
Samuel Gibson <sgibber2018@gmail.com>
2021-12-04 00:53:49 -0800

    Classifier.kt
    Refactoring. Removed some functions I'm no longer using.

--- src/main/kotlin/Classifier.kt
+++ src/main/kotlin/Classifier.kt
@@ -13,6 +13,13 @@ abstract class Classifier(
     val characteristics: List<Characteristic>,
     var score: Double = 0.0,
 ) {
+    var age = 0
+
+    /**
+     * Returns a copy of the classifier that is one generation older.
+     */
+    abstract fun emitSurvivor(): Classifier
+
     /**
      * Combines two Classifiers, producing two new Classifiers, with a chance for mutation and with the use of
      * "crossover", as described in John Holland's paper.
@@ -41,73 +48,10 @@ abstract class Classifier(
 }
 
 /**
- * Returns the number of Classifiers within a generation which are above the average score.
- *
- * Note that for now an above average performer is actually one with a below average score, since Prisoner's Dilemma
- * is like Golf in that the higher points are for worse outcomes. This means that when/if I generalize this I need
- * to come up with a more generic solution, probably.
- */
-fun numFit(
-    generation: List<Classifier>,
-    averageScore: Double,
-): Int {
-    return generation
-        .filter { it.score < averageScore }
-        .size
-}
-
-/**
- * Returns a string describing the most effective Classifier of the generation.
- */
-fun championString(generation: List<Classifier>): String {
-    val champion = generation.minByOrNull { it.score }!!
-    val numOfChampion = generation.filter { it.asBinaryString() == champion.asBinaryString() }.size
-    val percentGenePool = numOfChampion.toDouble() / generation.size * 100
-    return "Champion: ${champion.asBinaryString()}" +
-            "\n${champion.characteristics.map { it.prettyPrint() }}" +
-            "\n\tScore: ${champion.score}" +
-            "\n\t% of Gene Pool: $percentGenePool"
-}
-
-/**
  * Returns the average score of a generation.
  */
 fun averageScoreForGeneration(generation: List<Classifier>): Double {
     return generation.sumOf { it.score } / generation.size
-}
-
-/**
- * Takes a List<Classifier> and returns how many of them contain the active gene.
- */
-fun numWithActiveGene(generation: List<Classifier>, geneName: String): Int {
-    return generation.asSequence()
-        .map { it.characteristics }
-        .filter { it.any { it.name == geneName && it.active } }
-        .toList()
-        .size
-}
-
-// TODO: I can probably combine the following two into something cleaner. Will do soon.
-
-/**
- * Returns the % of the current generation that has an active copy of each gene.
- */
-fun activeGenePercentages(generation: List<Classifier>): Map<String, Double> {
-    val percentages = mutableMapOf<String, Double>()
-    generation.forEach { classifier ->
-        classifier.characteristics
-            .filter { it.active }
-            .forEach { characteristic ->
-                when (characteristic.name in percentages.keys) {
-                    true -> percentages[characteristic.name] = percentages[characteristic.name]!! + 1.0
-                    else -> percentages[characteristic.name] = 1.0
-                }
-            }
-    }
-    percentages.forEach { entry ->
-        percentages[entry.key] = entry.value / generation.size.toDouble() * 100.0
-    }
-    return percentages.toSortedMap()
 }
 
 /**
