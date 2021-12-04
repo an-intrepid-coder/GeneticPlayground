52a0911dc64de7f076458ef58bb10e444b608748
Samuel Gibson <sgibber2018@gmail.com>
2021-12-04 00:52:39 -0800

    Added support for Jetpack Compose.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,24 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.compose.compose
 
 plugins {
-    kotlin("jvm") version "1.6.0"
-    application
+    kotlin("jvm") version "1.5.31"
+    id("org.jetbrains.compose") version "1.0.0-alpha4-build362"
 }
 
 group = "me.sgibber2018"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    google()
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.5.31")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
+    implementation(compose.desktop.currentOs)
 }
 
 tasks.test {
@@ -25,6 +29,8 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-application {
-    mainClass.set("MainKt")
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,8 @@
-
 rootProject.name = "GeneticPlayground"
 
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
