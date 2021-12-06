d5dd43f043bb05efc2f1375a00389399bb3330e9
Samuel Gibson <sgibber2018@gmail.com>
2021-12-06 15:40:41 -0800

    In lieu of using GlobalScope, which seemed irresponsible, I've written a placeholder CoroutineHandler which allows me to do more or less the same thing. It is still a placeholder until I know more about idiomatic structured concurrency.

---
+++ src/main/kotlin/CoroutineHandler.kt
@@ -0,0 +1,38 @@
+import kotlinx.coroutines.*
+import kotlin.system.exitProcess
+
+/**
+ * This class is my attempt at a rudimentary coroutine handler. Will improve it as I learn more about how to do
+ * coroutines right. What it does is hold all active jobs and some functions to shut everything down
+ * gracefully if needed. Currently, this is just a home-rolled replacement for GlobalScope, and a temporary
+ * measure until I learn more.
+ */
+@OptIn(ObsoleteCoroutinesApi::class)
+class CoroutineHandler {
+    val coroutineScope = CoroutineScope(newFixedThreadPoolContext(8, "appThreads"))
+    var activeJobs = mutableListOf<Job>()
+    var cancelled = false
+
+    fun addJob(job: Job) {
+        activeJobs.add(job)
+    }
+
+    /**
+     * Pauses execution until all coroutines have joined and then clears the list of active jobs.
+     */
+    suspend fun joinAndClearActiveJobs() {
+        activeJobs.forEach { it.join() }
+        activeJobs = mutableListOf()
+    }
+
+    /**
+     * This attempts to cancel all coroutines before exiting the application. It works, most of the time.
+     * It could be better and I will improve it.
+     */
+    suspend fun cancelAndExit() {
+        cancelled = true
+        delay(1000)
+        activeJobs.forEach { it.cancelAndJoin() }
+        exitProcess(0)
+    }
+}
