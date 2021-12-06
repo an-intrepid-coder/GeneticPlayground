fb192f0539dbc238d238eaff2aac13757145afaf
Samuel Gibson <sgibber2018@gmail.com>
2021-12-06 15:42:02 -0800

    Main.kt
    The main application now uses the CoroutineHandler to gracefully exit the program. This is just in case the user closes the window while a bunch of coroutines are running in the background.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,21 +6,18 @@ import androidx.compose.ui.window.rememberWindowState
 import kotlinx.coroutines.*
 
 /**
- * Runs a given number of generations, checking for fitness and reproducing the most fit every time. Currently, only
- * runs Prisoner's Dilemma. Will do more eventually.
+ * Currently, runs the Prisoner's Dilemma demo/experiment. Will do more eventually.
  */
-@OptIn(DelicateCoroutinesApi::class)
+@OptIn(ObsoleteCoroutinesApi::class)
 fun main(args: Array<String>) = application {
-    // It is not recommended to use Global Scope. I will look in to alternatives soon:
-    val coroutineScope = GlobalScope
-
+    val coroutineHandler = CoroutineHandler()
     Window(
-        onCloseRequest = ::exitApplication,
+        onCloseRequest = { coroutineHandler.coroutineScope.launch { coroutineHandler.cancelAndExit() } },
         title = "Genetic Playground",
         state = rememberWindowState(width = 1200.dp, height = 700.dp)
     ) {
         MaterialTheme {
-            PrisonersDilemmaPlaygroundApp(coroutineScope)
+            PrisonersDilemmaPlaygroundApp(coroutineHandler)
         }
     }
 }
