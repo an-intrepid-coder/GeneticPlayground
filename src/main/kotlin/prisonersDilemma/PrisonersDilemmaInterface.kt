fb891fb03bc2db0d8b27b075d8099e6673967223
Samuel Gibson <sgibber2018@gmail.com>
2021-12-10 19:16:39 -0800

    PrisonersDilemmaInterface.kt
    Implemented a simpler interface for now.

---
+++ src/main/kotlin/prisonersDilemma/PrisonersDilemmaInterface.kt
@@ -0,0 +1,58 @@
+package prisonersDilemma
+
+import core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.*
+
+/**
+ * This app is very simple. The Playground is the back end and runs the simulation and does all the work,
+ * while this functions displays what is happening. I have kept it simple for now, but I will
+ * make this more complex in the future.
+ */
+@OptIn(ObsoleteCoroutinesApi::class)
+@Composable
+fun PrisonersDilemmaInterface(
+    playground: PrisonersDilemmaPlayground,
+    coroutineHandler: CoroutineHandler
+) {
+    LazyColumn(
+        modifier = Modifier
+            .background(Black)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        item {
+            Button(
+                onClick = {
+                    val coroutineScope = coroutineHandler.coroutineScope
+                    if (!playground.started)
+                        coroutineScope.launch {
+                            playground.runSimulation(coroutineHandler)
+                        }
+                },
+                content = {
+                    when (playground.started) {
+                        true -> when (playground.finished) {
+                            true -> Text("Finished!")
+                            else -> Text("...running...")
+                        }
+                        else -> Text("Start")
+                    }
+                }
+            )
+            WavyBorder()
+            LabeledText("Time Step", playground.timeStep.toString())
+            WavyBorder()
+            LabeledText("Avg Score vs. Random", playground.averageScoreAgainstRandom.toString())
+            LabeledText("Win % vs. Random", playground.averageWinPercentAgainstRandom.toString())
+        }
+    }
+}
