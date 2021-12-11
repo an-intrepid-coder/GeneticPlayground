f0e0b5f33423fbe4e3ddb2b4323c6f08d76f3296
Samuel Gibson <sgibber2018@gmail.com>
2021-12-10 19:14:42 -0800

    Main.kt
    Small changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-import prisonersDilemma.PrisonersDilemmaApp
+import prisonersDilemma.PrisonersDilemmaInterface
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -7,24 +7,28 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import core.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.launch
+import prisonersDilemma.PrisonersDilemmaPlayground
 
 /**
  * Currently, runs the Prisoner's Dilemma demo/experiment. Will do more eventually.
  */
-@OptIn(ObsoleteCoroutinesApi::class)
 fun main(args: Array<String>) = application {
+    val prisonersDilemmaPlayground = PrisonersDilemmaPlayground()
     val coroutineHandler = CoroutineHandler()
     Window(
-        onCloseRequest = { coroutineHandler.coroutineScope.launch { coroutineHandler.cancel(true) } },
+        onCloseRequest = { coroutineHandler
+            .coroutineScope
+            .launch { coroutineHandler.cancel(exit = true) }
+        },
         title = "Genetic Playground",
-        state = rememberWindowState(width = 1200.dp, height = 700.dp)
+        state = rememberWindowState(width = 800.dp, height = 600.dp)
     ) {
         val interfaceMode = remember { mutableStateOf(InterfaceMode.MENU) }
         MaterialTheme {
             when (interfaceMode.value) {
                 InterfaceMode.MENU -> MainMenu(interfaceMode)
-                InterfaceMode.PRISONERS_DILEMMA -> PrisonersDilemmaApp(coroutineHandler)
+                InterfaceMode.PRISONERS_DILEMMA -> PrisonersDilemmaInterface(prisonersDilemmaPlayground, coroutineHandler)
             }
         }
     }
